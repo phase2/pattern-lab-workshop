fabda9f72922442ff7e12e63aeea74a16caaac8a
Tanner Heffner <theffner@phase2technology.com>
2015-08-05 12:49:26 -0700

    add stuff to verify exercise

--- exercises/create_an_atom/exercise.js
+++ exercises/create_an_atom/exercise.js
@@ -5,9 +5,7 @@ var exercise = require('workshopper-exercise')();
 var filecheck = require('workshopper-exercise/filecheck');
 var execute = require('workshopper-exercise/execute');
 var comparestdout = require('workshopper-exercise/comparestdout');
-
-
-// checks if a file exists, named a certain way, containing the mustache we want
+var fs = require('fs');
 
 // checks that the submission file actually exists
 exercise = filecheck(exercise);
@@ -17,5 +15,16 @@ exercise = execute(exercise);
 
 // compare stdout of solution and submission
 exercise = comparestdout(exercise);
+
+// checks if a file exists, named a certain way, containing the mustache we want
+fs.readFile('/atoms/01-tutorial-button.mustache', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  if((data.indexOf('<button>') !== -1) & (data.indexOf('</button>') !== -1)) {
+    console.log(data);
+  }
+}); //data is the contents of the file, for this lesson we want it to have: <button></button>
+
 
 module.exports = exercise;
