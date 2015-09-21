8fc3eff0fdb7488e58e667af1d2bfde5586706dd
Tanner Heffner <theffner@phase2technology.com>
2015-09-21 16:13:58 -0700

    fix templates lesson

--- problems/templates/solution.js
+++ problems/templates/solution.js
@@ -1,5 +1,5 @@
 console.log("File in templates folder!");
-console.log('There\'s wrapper divs!);
+console.log('There\'s wrapper divs!');
 console.log('Header partial found!');
 console.log('Product grid partial exists!');
 console.log('Footer partial is present!');
